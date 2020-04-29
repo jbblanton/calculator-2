e32878e4d1950f72433e870296aeffea320173d4
jbblanton <jessicabblanton@gmail.com>
2020-04-29 23:06:46 +0000

    Yes to tokens

--- calculator.py
+++ calculator.py
@@ -3,5 +3,16 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+math_equation = input("Enter your equation: ").split(" ")
 
-# Replace this with your code
+def calculator(function, *num):
+    """Interface for a calculator
+
+    receives a string, breaks it into usable parts, outputs a number"""
+
+
+
+
+    return [function, num]
+
+print(calculator(math_equation))
