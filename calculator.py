e0567033b2083b4d650d5f6d42450ba55a18ca4a
jbblanton <jessicabblanton@gmail.com>
2020-04-29 23:59:52 +0000

    add through divide

--- calculator.py
+++ calculator.py
@@ -3,16 +3,41 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-math_equation = input("Enter your equation: ").split(" ")
 
-def calculator(function, *num):
+
+def calculator():
     """Interface for a calculator
 
     receives a string, breaks it into usable parts, outputs a number"""
+    quit = False
+    
+    while quit is False: 
+        math_equation = input("Enter your equation: ").split(" ")
 
+        action = str(math_equation[0])
+        num1 = float(math_equation[1])
+        num2 = float(math_equation[2])
+        #num3 = float(math_equation[3])
 
+        if action == '+':
+            print(add(num1, num2))
+        elif action == '-':
+            print(subtract(num1, num2))
+        elif action == '*':
+            print(multiply(num1, num2))
+        elif action == '/':
+            print(divide(num1, num2))
+        elif action == 'square':
+            print(square(num1))
+        elif action == 'cube':
+            return num1 * num1 * num1
+        elif action == 'power':
+            return num1 ** num2
+        elif action == 'mod':
+            return num1 % num2
+        #else action.lower() == "q" or action == "quit":
+        #    break
 
+    return "not working"
 
-    return [function, num]
-
-print(calculator(math_equation))
+print(calculator())
