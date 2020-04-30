4471a81b9e72dfdada6c3ae7170944e4af765cb2
jbblanton <jessicabblanton@gmail.com>
2020-04-30 01:38:34 +0000

    Completed

--- calculator.py
+++ calculator.py
@@ -15,44 +15,34 @@ def calculator():
         math_equation = input("Enter your equation: ").split(" ")
 
         action = str(math_equation[0])
-
-        if action == "q" or action == "quit":
-            break
-
-        elif action == '+':
+        if len(math_equation) >= 2:
             num1 = float(math_equation[1])
-            num2 = float(math_equation[2])
+
+        if len(math_equation) == 3:
+            num2 = float(math_equation[2])    
+
+        # if action == "q" or action == "quit":
+        #     quit = True
+
+        if action == '+':
             print(add(num1, num2))
         elif action == '-':
-            num1 = float(math_equation[1])
-            num2 = float(math_equation[2])
             print(subtract(num1, num2))
         elif action == '*':
-            num1 = float(math_equation[1])
-            num2 = float(math_equation[2])
             print(multiply(num1, num2))
         elif action == '/':
-            num1 = float(math_equation[1])
-            num2 = float(math_equation[2])
             print(divide(num1, num2))
         elif action == 'square':
-            num1 = float(math_equation[1])
             print(square(num1))
         elif action == 'cube':
-            num1 = float(math_equation[1])
             print(cube(num1))
         elif action == 'pow':
-            num1 = float(math_equation[1])
-            num2 = float(math_equation[2])
             print(power(num1, num2))
         elif action == 'mod':
-            num1 = float(math_equation[1])
-            num2 = float(math_equation[2])
             print(mod(num1, num2))
         else:
-            action == "q" or action == "quit"
-            break
+            quit = True
 
-    return "not working"
+    return "Bye!"
 
 print(calculator())
